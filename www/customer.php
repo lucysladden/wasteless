886737884575f52cf9acfbec75d8ce770a6ac95b
lucysladden <lj.sladden@gmail.com>
2022-08-22 20:47:06 +1200

    made a (rough) start on php functionality.

--- www/customer.php
+++ www/customer.php
@@ -9,5 +9,53 @@
   <body>
     <p>Display list of available food items</p>
     <a href="/index.php">Home</a>
+
+<h2> Admin </h2>
+<main>
+    <h3>Food Entrys: </h3>
+    <?php
+    $db_host   = '192.168.56.12';
+    $db_name   = 'fvision';
+    $db_user   = 'webuser';
+    $db_passwd = 'insecure_db_pw';
+    
+    $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+    
+    $conn = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+
+    $sql = "SELECT * FROM food_entry";
+
+    $result = $conn->query($sql);
+
+        // output data of each row
+      echo "<table>
+
+          <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>address</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Time Frame</th>
+          </tr>";
+        while($row = $q->fetch()) {
+
+          echo "<form action = deleteEntry.php method=post>";
+          echo '<input type ="hidden" name="entryID" value=' . $row['entryID'] . '>';
+          echo "<tr>";
+          echo "<td>" . $row['entryID'] . "</td>";
+          echo "<td>" . $row['name'] . "</td>";
+          echo "<td>" . $row['address'] . "</td>";
+          echo "<td>" . $row['description'] . "</td>";
+          echo "<td>" . $row['price'] . "</td>";
+          echo "<td>" . $row['timeframe'] . "</td>";
+          echo "<td>" . " <input type = 'submit' id = 'entries' name = delete  value = " . 'delete' . " > </td>";
+          echo "</form>";
+      }
+  
+
+    ?>
+</main>
   </body>
 </html>
--- www/restaurant-form.php
+++ www/restaurant-form.php
@@ -7,6 +7,42 @@
     <title>Document</title>
   </head>
   <body>
+  <main>
+    <?php
+    $db_host   = '192.168.56.12';
+    $db_name   = 'fvision';
+    $db_user   = 'webuser';
+    $db_passwd = 'insecure_db_pw';
+
+    $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+    $conn = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+    if (isset($_POST['reasturant-form'])) {
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+        $description = $_POST['description'];
+        $price = $_POST['price'];
+        $timeframe = $_POST['timeframe'];
+
+        $query = "INSERT INTO food_entry(name,address,length,description,price,timeframe) VALUES (" . "'" . $name . "'" . "," . "'" . $address . "'" . "," . "'" . $length . "'" . "," . "'" . $description . "'" . "," . "'" . $price . "'" . "," . "'" . $timeframe . "'" . ");";
+        
+        if (isset($conn)) {
+          $conn->query($query);
+        }
+
+          
+        echo '<p>entry Submitted</p>';
+        header('Location: restaurant-form.php');
+        exit;
+
+      }
+  
+    ?>
+
+
+
+    
     <h1>Food Donation Form</h1>
     <form action="images/Pont_du_Gard_BLS.jpeg">
       <table>
@@ -48,5 +84,7 @@
       </table>
     </form>
     <a href="/index.php">Home</a>
+
+    </main>
   </body>
 </html>
